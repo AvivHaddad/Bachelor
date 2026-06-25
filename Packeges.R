eb8c5005d9e6b628538d474f28ff6ccf706da2d0
AvivHaddad <159770372+AvivHaddad@users.noreply.github.com>
2026-06-25 13:55:27 +0200

    Add installation for additional R packages
    Added installation commands for additional R packages including gplots, ggrepel, clusterProfiler, pathview, enrichplot, fgsea, biomaRt, and DT.

--- Packeges.R
+++ Packeges.R
@@ -8,11 +8,20 @@ BiocManager::install("edgeR")
 BiocManager::install("GSEABase")
 BiocManager::install("org.Hs.eg.db")
 BiocManager::install("DESeq2")
-install(dplyr)
+install.packages(dplyr)
 BiocManager::install('seandavi/GEOquery')
 
-if (!require('remotes')) install.packages('remotes'); # make sure you have Rtools installed first! if not, then run:
+if (!require('remotes')) install.packages('remotes') # make sure you have Rtools installed first! if not, then run:
 #install.packages('installr')
 #install.Rtools()
 remotes::install_github('talgalili/installr')
 
+install(gplots)
+install(ggrepel)
+
+BiocManager::install("clusterProfiler")
+BiocManager::install("pathview")
+BiocManager::install("enrichplot")
+BiocManager::install("fgsea")
+BiocManager::install("biomaRt")
+install("DT")
