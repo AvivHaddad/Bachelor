42f78c1acc6f8b67c1c61ff587958cea0b0581b6
AvivHaddad <159770372+AvivHaddad@users.noreply.github.com>
2026-06-28 15:53:17 +0200

    Refactor data loading to use RDS files
    Refactored code to read data from saved RDS files instead of fetching from GEO. Updated data processing steps for DGEList creation.

--- BI-Polar_start_groupping.R
+++ BI-Polar_start_groupping.R
@@ -1,28 +1,29 @@
 
 my_id <- "GSE82042"
-gse <- getGEO(my_id, returnType = 'ExpressionSet')
+#gse <- getGEO(my_id, returnType = 'ExpressionSet')
 
-length(gse)
-gse <- gse[[1]]
-gse
-
-pData(gse)
+#gse <- gse[[1]]
+#saveRDS(gse,"C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/gse")
+gse <- readRDS("C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/gse")
 fdata <- fData(gse)
-
-
-pData(gse)$data_processing[1]
-summary(exprs(gse))
-head(exprs(gse))
 prob_IDs <- rownames(exprs(gse))
-sum(is.na(gse@featureData@data[["Symbol"]]))
-head(gse@featureData@data[["Symbol"]])
-identical(rownames(exprs(gse)), rownames(gse@featureData@data))
+#pheno <- as.data.frame(gse@phenoData@data)
+#saveRDS(pheno, "C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/pheno")
+pheno <- readRDS("C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/pheno")
 
-#Don't really need the rest
-BI_GEO_Accession <- gse@phenoData@data[["geo_accession"]]
-BI_Gender <- gse@phenoData@data[["characteristics_ch1.5"]]
-BI_Expression_data <- exprs(gse)
-BI_Sickness_Status <- gse@phenoData@data[["characteristics_ch1.4"]]
+
+#BI_GEO_Accession <- gse@phenoData@data[["geo_accession"]]
+#saveRDS(BI_GEO_Accession, "C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/BI_GEO_Accession")
+BI_GEO_Accession <- readRDS("C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/BI_GEO_Accession")
+#BI_Gender <- gse@phenoData@data[["characteristics_ch1.5"]]
+#saveRDS(BI_Gender , "C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/BI_Gender")
+BI_Gender <- readRDS("C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/BI_Gender")
+#BI_Expression_data <- exprs(gse)
+#saveRDS(BI_Expression_data, "C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/BI_Expression_data")
+BI_Expression_data <- readRDS("C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/BI_Expression_data")
+#BI_Sickness_Status <- gse@phenoData@data[["characteristics_ch1.4"]]
+#saveRDS(BI_Sickness_Status, "C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/BI_Sickness_Status")
+BI_Sickness_Status <- readRDS("C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/BI_Sickness_Status")
 
 
 BiFullData <- data.frame(BI_GEO_Accession,BI_Gender,BI_Sickness_Status)
@@ -45,20 +46,26 @@ Expression_BPFemale <- exprs(gse)[, BI_Positive_female$BI_GEO_Accession]
 Expression_BNFemale <- exprs(gse)[, BI_Negative_female$BI_GEO_Accession]
 Expression_BPMale <- exprs(gse)[, BI_Positive_male$BI_GEO_Accession]
 Expression_BNMale <- exprs(gse)[, BI_Negative_male$BI_GEO_Accession]
-#Except for this:
-Genes <- gse@featureData@data[["Symbol"]]
-genXID <- data.frame(Genes, prob_IDs)
+
+
+#Genes <- gse@featureData@data[["Symbol"]]
+#saveRDS(Genes, "C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/Genes")
+Genes <- readRDS("C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/Genes")
+
+#genXID <- data.frame(Genes, prob_IDs)
+#saveRDS(genXID, "C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/genXID")
+genXID <- readRDS("C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/genXID")
 
 BI_meta <- data.frame(SampleID = BI_GEO_Accession,
                       sex = BI_Gender)
 rownames(BI_meta) <- BI_meta$SampleID
+saveRDS(BI_meta,"C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/BI_meta")
+BI_meta <- readRDS("C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/BI_meta")
 
 BI_dge <- DGEList(counts = BI_Expression_data,
                   genes = genXID,
                   samples = BI_meta)
-#I don't need to use voom, my data is already log2 transformed
-dge_v <- voom(BI_dge, plot = TRUE)
-saveRDS(dge_v, file = "../dge_v.rds")
+
 
 
 BI_dge_1 <- DGEList(counts = BI_Expression_data,
@@ -66,19 +73,14 @@ BI_dge_1 <- DGEList(counts = BI_Expression_data,
                   samples = BI_meta,
                  group = BI_meta$sex,
                  annotation.columbs = Genes)
-dge_v_1 <- voom(BI_dge_1, plot = TRUE)
-saveRDS(dge_v_1, file = "../dge_v_1.rds")
 
 Diagnosis <-  BI_Sickness_Status
 BI_meta_Diag <- cbind(BI_meta,Diagnosis)
-
 
 BI_dge_2 <- DGEList(counts = BI_Expression_data,
                     genes = genXID,
                     samples = BI_meta_Diag,
                     group = BI_meta_Diag$Diagnosis)
-dge_v_2 <- voom(BI_dge_2, plot = TRUE)
-saveRDS(dge_v_2, file = "../dge_v_2.rds")
 
 
 Diag_Gender_combo <- paste(BI_Gender, BI_Sickness_Status)
@@ -87,5 +89,7 @@ BI_dge_3 <- DGEList(counts = BI_Expression_data,
                     genes = genXID,
                     samples = BI_meta_Diag,
                     group = BI_meta_combo$Diag_Gender_combo)
-dge_v_3 <- voom(BI_dge_2, plot = TRUE)
-saveRDS(dge_v_3, file = "../dge_v_3.rds")
+
+saveRDS(BI_dge_3 , "C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/BI_dge_3 ")
+BI_dge_3  <- readRDS("C:/Users/avive/Documents/Thesis/R code/Thesis/Bachlor/Working/Reusable Files/BI_dge_3 ")
+#-------------------------------------------------------------------------------------------------------#
